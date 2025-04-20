d709305b379b1842143b988e80ed1fed76eb6692
Vinicius Rodrigues <viniciusrodrigues@Viniciuss-MacBook-Pro.local>
2025-04-20 16:52:38 +0700

    .gitignore and properties

---
+++ backend/src/main/java/com/easymenu/EasyMenuApplication.java
@@ -0,0 +1,13 @@
+package com.easymenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyMenuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/easymenu/EasyMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easymenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
