0e86b394bca95c0de69308fbd18ddab58c041be6
Vinicius Rodrigues <viniciusrodrigues@Viniciuss-MacBook-Pro.local>
2025-08-04 17:09:55 +0800

    Update Redis configuration

--- backend/src/main/java/com/easymenu/config/RedisConfiguration.java
+++ backend/src/main/java/com/easymenu/config/RedisConfiguration.java
@@ -1,6 +1,5 @@
 package com.easymenu.config;
 
-import com.easymenu.product.ProductResponseDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
